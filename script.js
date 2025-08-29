a8ba2a3c19c2d9b8d46c7f2f9922e74a83bd0e17
Vivek Gurung <virguru01@gmail.com>
2025-08-29 10:25:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//JS
+  
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('nav ul');
+toggle.addEventListener('click', () => {
+navLinks.classList.toggle('show');
+});        
+          
