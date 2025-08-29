d3ddef7bae9cd96e45f61b21f388fe870a5467bd
Vivek Gurung <virguru01@gmail.com>
2025-08-29 10:56:27 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
   
 const toggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('nav ul');
+
 toggle.addEventListener('click', () => {
-navLinks.classList.toggle('show');
-});        
-          
+  navLinks.classList.toggle('show');
+});
+
+// Close menu after clicking any nav link
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
